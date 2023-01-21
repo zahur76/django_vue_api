b8cb18b368b55b277217c39c96d614bafe5486a3
zahur76 <zahurmeerun@example.com>
2023-01-21 16:20:42 +0000

    add: character and faction model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Faction, Character
 
 # Register your models here.
+admin.site.register(Faction)
+admin.site.register(Character)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
 # Create your models here.
+class Faction(models.Model):
+
+    class Meta:
+        verbose_name = "Faction"
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+# Create your models here.
+class Character(models.Model):
+    class Meta:
+        verbose_name = "Character"
+
+    faction = models.ForeignKey(
+            'Faction', null=False, blank=False, on_delete=models.CASCADE,
+            related_name='characters')
+    name = models.CharField(max_length=254)
+    GENDER = [
+                ('Male', 'Male'),
+                ('Female', 'Female'),
+                ('Other', 'Other'), ]
+    gender = models.CharField(max_length=6, choices=GENDER, null=False)
+    birth_year = models.CharField(max_length=254)
+    birth_planet = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
